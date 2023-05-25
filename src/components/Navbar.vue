1ca8aa5a04b2ab5a9c1a5a391a69a67c8d47407d
alextassy24 <t_alex_00@yahoo.com>
2023-05-26 02:50:02 +0300

    Added vuetify and tailwind

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template v-slot:append>
+	<v-app-bar scroll-behavior="collapse">
+		<v-row class="justify-between bg-blue-200">
+			<v-btn class="m-5 bg-blue-500">Not enough jobs</v-btn>
+
+			<v-btn icon class="m-5">
+				<v-icon>mdi-dots-vertical</v-icon>
+			</v-btn>
+		</v-row>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	name: "Navbar",
+};
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,9 @@
+// Styles
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+// Vuetify
+import { createVuetify } from "vuetify/lib/framework";
+
+export default createVuetify();
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
